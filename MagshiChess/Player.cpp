2f81184c76ad50dbcfcbf0fa6aa988fc91ea886e
Ariel Adjadj <arieladjadj1@gmail.com>
2019-12-25 17:01:42 +0200

    Update Player class

--- MagshiChess/Player.cpp
+++ MagshiChess/Player.cpp
@@ -1,7 +1,9 @@
 #include "Player.h"
 
-Player::Player() //c'tor
+Player::Player(Color c, std::string& startingBoard) //c'tor
 {
+	this->_color = c;
+	this->_board = PipeInputOperations::generateBoard(this->_color, startingBoard);
 }
 
 Player::~Player()  //d'tor
@@ -10,52 +12,41 @@ Player::~Player()  //d'tor
 	delete[] this->_board;
 }
 
-void Player::setPipe(Pipe p)
-{
-	this->_p = p;
-}
-
 void Player::makeMove(std::string& msgFromGraphics)
 {
-
-	error_level_code move_code = isValidCMD(msgFromGraphics);
-
-	if (move_code == valid || move_code == valid_check || move_code == check_mate)
-	{
-
-	}
-	char msgToGraphics[1024];
-	msgToGraphics[0] = (char)(move_code + '0');
-	msgToGraphics[1] = 0;
-
-	this->_p.sendMessageToGraphics(msgToGraphics);
+	int srcRow, srcCol, dstRow, dstCol;
+	std::tie(srcRow, srcCol, dstRow, dstCol) = PipeInputOperations::moveToPos(msgFromGraphics);
+	Piece* temp = this->_board[srcRow][srcCol];
+	this->_board[dstRow][dstCol] = temp;
+	this->_board[srcRow][srcCol] = nullptr;
 
 }
 error_level_code Player::isValidCMD(std::string& msgFromGraphics) const
 {
 	int srcRow, srcCol, dstRow, dstCol;
 	std::tie(srcRow, srcCol, dstRow, dstCol) = PipeInputOperations::moveToPos(msgFromGraphics);
-	error_level_code response_code = valid; 
+	error_level_code response_code = valid;
 
-	
 	//check error_level_code 2 - in src position there is no piece of curr player
 	if (this->_board[srcRow][srcCol] == nullptr || this->_board[srcRow][srcCol]->getColor() != this->_color)
 	{
 		response_code = invalid_src_not_occupied;
 	}
-	else if (this->_board[dstRow][dstCol]->getColor() == this->_color) //check error_level_code 3 - in dst position there is piece of curr player
+	//check error_level_code 3 - in dst position there is piece of curr player
+	else if (this->_board[dstRow][dstCol] != nullptr && this->_board[dstRow][dstCol]->getColor() == this->_color)
 	{
 		response_code = invalid_dst_occupied;
 	}
-	else if (!this->_board[srcRow][srcCol]->validMove(this->_board, msgFromGraphics)) //check error_level_code 6 - invalid move of piece
+	//check error_level_code 6 - invalid move of piece
+	else if (this->_board[srcRow][srcCol] != nullptr && !this->_board[srcRow][srcCol]->validMove(this->_board, msgFromGraphics))
 	{
-		response_code = invalid_behevior; 
+		response_code = invalid_behevior;
 	}
 	else if (srcRow == dstRow && srcCol == dstCol)  //check error_level_vode 7 - dst posiiton and src position are same
 	{
 		response_code = invlaid_same_src_dst;
 	}
-	else
+	else   //TODO: add check to check and mate
 	{
 		return valid;
 	}
