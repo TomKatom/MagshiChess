bed1df285ca841af8570b36368339e360a59cee6
Ariel Adjadj <arieladjadj1@gmail.com>
2019-12-31 03:08:57 +0200

    Akmost finish server side

--- chessServer/ServerFunctions.cpp
+++ chessServer/ServerFunctions.cpp
@@ -83,9 +83,8 @@ void ServerFunctions::forward_msg(sf::TcpSocket* sock1, sf::TcpSocket* sock2) //
 		else
 		{
 			if (ChessProtocol::isMoveCmd(data))
-			{
 				ChessProtocol::convertMoveCmd(data);
-			}
+			
 			if (sock2->send(data, strlen(data) + 1) != sf::Socket::Done)
 			{
 				ServerFunctions::sendDisconnectMsg(sock1);
