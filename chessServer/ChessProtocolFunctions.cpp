fc7511d60d49cc8e160c689edda1d2326cf75a88
Tom Chengich <tomcengich@gmail.com>
2019-12-31 19:30:38 +0200

    Minor changes

--- chessServer/ChessProtocolFunctions.cpp
+++ chessServer/ChessProtocolFunctions.cpp
@@ -1,13 +1,13 @@
 #include "ChessProtocolFunctions.hpp"
 #pragma warning(disable:4996)
-#include <cstring>
+#include <string>
 
 /*
 This function checks if the data that recived from the other player is movment cmd or not
 */
 bool ChessProtocol::isMoveCmd(char* data)
 {
-	return strncmp(data, "move", 4);
+	return std::string(data).find("move") != std::string::npos;
 }
 
 /*
@@ -15,6 +15,14 @@ This function convert from one player positions on board to another - in the mov
 */
 void ChessProtocol::convertMoveCmd(char* data)
 {
-
+	int srcRow = 0, srcCol = 0, dstRow = 0, dstCol = 0;
+	srcRow = data[5] - '0';
+	srcCol = data[7] - '0';
+	dstRow = data[9] - '0';
+	dstCol = data[11] - '0';
+	data[5] = (char)(7 - srcRow + '0');
+	data[7] = (char)(7 - srcCol + '0');
+	data[9] = (char)(7 - dstRow + '0');
+	data[11] = (char)(7 - dstCol + '0');
 }
 
